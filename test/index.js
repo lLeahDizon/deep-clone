bd04e38b7397b4c58b96276d0069536edb44f6bb
杨海涛 <leahyang@tungee.com>
2026-03-19 23:01:00 +0800

    feat: 拷贝正则

--- test/index.js
+++ test/index.js
@@ -68,5 +68,29 @@ describe('deepClone', () => {
       assert(a.name === a2.name)
       assert(a.self !== a2.self)
     });
+    xit ('不会爆栈', () => {
+      const a = {child: null}
+      let b = a
+      for (let i = 0; i < 20000; i++) {
+        b.child = {
+          child: null
+        }
+        b = b.child
+      }
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.child !== a2.child)
+    })
+    it('可以复制正则表达式', () => {
+      const a = new RegExp("h1\\d+", 'gi')
+      a.xxx = {yyy: {zzz: 1}}
+      const a2 = deepClone(a)
+      assert(a.source === a2.source)
+      assert(a.flags === a2.flags)
+      assert(a !== a2)
+      assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz)
+      assert(a.xxx.yyy !== a2.xxx.yyy)
+      assert(a.xxx !== a2.xxx)
+    });
   });
 })
